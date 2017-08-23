6b99fd2f0748fffba628ed97ae0a29285af92a96
NeilHu94 <hu604061840@163.com>
2017-08-23 16:04:47 +0800

    Create Numbers_aty.java

---
+++ src/main/java/com/demo/neilhu/dialect/Numbers_aty.java
@@ -0,0 +1,21 @@
+  package com.demo.neilhu.dialect;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
+import static com.demo.neilhu.dialect.R.mipmap.ic_launcher;
+public class Numbers_aty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new Numbers_frament()).commit();
+    }
+}
